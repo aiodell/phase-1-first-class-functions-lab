c4d829fd10fb3d1dace9a30ad3cbafb0b944dacf
Andrianna <andrianna.odell@gmail.com>
2022-06-29 08:52:51 -0400

    commit tree acb8aa80d0f862b7096774a4279e18dc06f2318b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+// create function to return the first two drivers
+const returnFirstTwoDrivers = function(drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return drivers.slice(0,2);
+}
+
+// create function to return the last two drivers
+const returnLastTwoDrivers = function(drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']){
+    return drivers.slice(-2);
+}
+
+// create an array containing two elements: the two functions defined
+const selectingDrivers = [returnFirstTwoDrivers(drivers), returnLastTwoDrivers(drivers)];
